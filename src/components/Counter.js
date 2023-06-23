3d7be3c328784385464666c809aa99bad89ea232
Mr-Psmith <psancthus@gmail.com>
2023-06-23 11:30:14 +0000

    progress

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,22 +5,29 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter) //we bring it over to use it ti check the truthyness of the value
 
   const incrementHandler = () => {
     dispatch({type: "increment"});
+  };
+  const increaseHandler = () => {
+    dispatch({type: "increase", valueProp: 5})
   };
   const decrementHandler = () => {
     dispatch({type: "decrement"});
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: "toggle"}); // we dispatch the action here
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>} {/* so it only show it it is truthy */}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/redux-index.js
+++ src/store/redux-index.js
@@ -1,15 +1,33 @@
 import {createStore} from "redux"; //or we can import {createStore} exactly
 
-const reducer = (state = {counter: 0}, action) => {
+const initialState = { counter: 0, showCounter: true}; //easier to read
+
+const reducer = (state = initialState, action) => {
 
     if (action.type === "increment") {
         return{
-        counter: state.counter +1
+        counter: state.counter +1,
+        showCounter: state.showCounter // have to set it, so we set to the value we currently having
         };
     }
+    if (action.type === "increase") {
+        return {
+            counter: state.counter + action.valueProp,
+            showCounter: state.showCounter
+    }
+    }
+
     if (action.type === "decrement") {
         return{
-        counter: state.counter -1
+        counter: state.counter -1,
+        showCounter: state.showCounter
+        };
+    }
+
+    if (action.type === "toggle") {
+        return {
+            showCounter: !state.showCounter, // we are setting it this way the opposite it was, so if it was true we are setting it false, and vica versa
+            counter:state.counter
         };
     }
 
