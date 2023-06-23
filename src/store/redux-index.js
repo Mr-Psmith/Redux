84389bc33cf1d333778f7deb273b30de75c29b85
Mr-Psmith <psancthus@gmail.com>
2023-06-23 17:36:01 +0000

    olmost fin 1st

--- src/store/redux-index.js
+++ src/store/redux-index.js
@@ -1,6 +1,26 @@
 import {createStore} from "redux"; //or we can import {createStore} exactly
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true}; //easier to read
+
+const counterSlice = createSlice({
+    name: "counter", //every slice needs a name(whateverWeWant) as identifier of that piece of state.
+    initialState: initialState, // mandatory as well, but we could just write initialState as this is th same in modern Js
+    reducers: { //Reducer is an object - a map you could say, of all the reducers this state slice needs.
+        increment(state) { //These methods will be called for you by Redux, and they will receive the current state. Also to the action, but actually here we don't need the action because these methods will automatically be called for you
+            state.counter++;                    //depending on which action was triggered. So we don't need to write our own if checks anymore instead we'll be able to identify these different reducers and dispatch actions that target these different reducers.
+        },          //This seems to be mutating the state, but it is not, bec when using Redux toolkit and its functions like create slice, we can't accidentally manipulate the existing state. 
+        decrement(state) {          //Because Redux toolkit internally uses another package, called imgur, which will detect code like this and which will automatically clone the existing state, create a new state object, keep all the state which we're not editing, and override the state which we are editing in an immutable way.
+            state.decrement--;
+        },             
+        increase(state, action) { //But now here if we need some data that's attached to the action, then we can still accept it as a parameter and use it in the reducer function in the reducer method.
+            state.counter = state.counter + action.amount; 
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
 
 const reducer = (state = initialState, action) => {
 
