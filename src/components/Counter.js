5b797c75f7179e255dfdb360bdf1ee2444487f55
Mr-Psmith <psancthus@gmail.com>
2023-06-24 16:12:31 +0000

    finished1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from "react-redux"; //custom redux hook
 
+import { counterActions } from "../store/counter";
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
@@ -7,20 +9,33 @@ const Counter = () => {
   const counter = useSelector(state => state.counter);
   const show = useSelector(state => state.showCounter) //we bring it over to use it ti check the truthyness of the value
 
+  // const incrementHandler = () => {
+  //   dispatch({type: "increment"});
+  // };
+  // const increaseHandler = () => {
+  //   dispatch({type: "increase", valueProp: 5})
+  // };
+  // const decrementHandler = () => {
+  //   dispatch({type: "decrement"});
+  // };
+
+  // const toggleCounterHandler = () => {
+  //   dispatch({type: "toggle"}); // we dispatch the action here
+  // };
+
   const incrementHandler = () => {
-    dispatch({type: "increment"});
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({type: "increase", valueProp: 5})
+    dispatch(counterActions.increase(5)) //{ type: SOME_UNIQUE_IDENTIFIER, payload: 5 }=>this is how it looks behind the scenes, payload is given
   };
   const decrementHandler = () => {
-    dispatch({type: "decrement"});
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({type: "toggle"}); // we dispatch the action here
+    dispatch(counterActions.toggleCounter()); // we dispatch the action here
   };
-
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
--- src/store/redux-index.js
+++ src/store/redux-index.js
@@ -1,69 +1,102 @@
-import {createStore} from "redux"; //or we can import {createStore} exactly
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+// //import {createStore} from "redux"; //or we can import {createStore} exactly
+// import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true}; //easier to read
+// const initialCounterState = { counter: 0, showCounter: true}; //easier to read
 
-const counterSlice = createSlice({
-    name: "counter", //every slice needs a name(whateverWeWant) as identifier of that piece of state.
-    initialState: initialState, // mandatory as well, but we could just write initialState as this is th same in modern Js
-    reducers: { //Reducer is an object - a map you could say, of all the reducers this state slice needs.
-        increment(state) { //These methods will be called for you by Redux, and they will receive the current state. Also to the action, but actually here we don't need the action because these methods will automatically be called for you
-            state.counter++;                    //depending on which action was triggered. So we don't need to write our own if checks anymore instead we'll be able to identify these different reducers and dispatch actions that target these different reducers.
-        },          //This seems to be mutating the state, but it is not, bec when using Redux toolkit and its functions like create slice, we can't accidentally manipulate the existing state. 
-        decrement(state) {          //Because Redux toolkit internally uses another package, called imgur, which will detect code like this and which will automatically clone the existing state, create a new state object, keep all the state which we're not editing, and override the state which we are editing in an immutable way.
-            state.decrement--;
-        },             
-        increase(state, action) { //But now here if we need some data that's attached to the action, then we can still accept it as a parameter and use it in the reducer function in the reducer method.
-            state.counter = state.counter + action.amount; 
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        }
-    }
-});
-
-// const reducer = (state = initialState, action) => {
-
-//     if (action.type === "increment") {
-//         return{
-//         counter: state.counter +1,
-//         showCounter: state.showCounter // have to set it, so we set to the value we currently having
-//         };
+// const counterSlice = createSlice({
+//     name: "counter", //every slice needs a name(whateverWeWant) as identifier of that piece of state.
+//     initialState: initialCounterState, // mandatory as well, but we could just write initialState as this is th same in modern Js
+//     reducers: { //Reducer is an object - a map you could say, of all the reducers this state slice needs.
+//         increment(state) { //These methods will be called for you by Redux, and they will receive the current state. Also to the action, but actually here we don't need the action because these methods will automatically be called for you
+//             state.counter++;                    //depending on which action was triggered. So we don't need to write our own if checks anymore instead we'll be able to identify these different reducers and dispatch actions that target these different reducers.
+//         },          //This seems to be mutating the state, but it is not, bec when using Redux toolkit and its functions like create slice, we can't accidentally manipulate the existing state. 
+//         decrement(state) {          //Because Redux toolkit internally uses another package, called imgur, which will detect code like this and which will automatically clone the existing state, create a new state object, keep all the state which we're not editing, and override the state which we are editing in an immutable way.
+//             state.decrement--;
+//         },             
+//         increase(state, action) { //But now here if we need some data that's attached to the action, then we can still accept it as a parameter and use it in the reducer function in the reducer method.
+//             state.counter = state.counter + action.payload; //action.payload as this is the name react given to the vlue in counter.js
+//         },
+//         toggleCounter(state) {
+//             state.showCounter = !state.showCounter;
+//         }
 //     }
-//     if (action.type === "increase") {
-//         return {
-//             counter: state.counter + action.valueProp,
-//             showCounter: state.showCounter
-//     }
-//     }
-
-//     if (action.type === "decrement") {
-//         return{
-//         counter: state.counter -1,
-//         showCounter: state.showCounter
-//         };
-//     }
-
-//     if (action.type === "toggle") {
-//         return {
-//             showCounter: !state.showCounter, // we are setting it this way the opposite it was, so if it was true we are setting it false, and vica versa
-//             counter:state.counter
-//         };
-//     }
-
-//     return state;
+// });
 
 
+// const initialAuthState = {
+//     isAuthenticated: false
 // };
+// const authSlice = createSlice({
+//     name: "auth",
+//     initialState: initialAuthState,
+//     reducers: {
+//         login(state) {
+//             state.isAuthenticated = true;
+//         },
+//         logout(state) {
+//             state.isAuthenticated = false;
+//         }
+//     }
+// }); 
 
-//const store = createStore(counterSlice.reducer); //This would be good this way for a smaller app.
-//But if we have a bigger app with multiple stateslices, than we would have a problem as there can be only one reducer passed to create store
-const store = configureStore({ //we pass a configuration obj, - which is expected. And then we set a reducer property => expected property by configureStore.
-    reducer: counterSlice.reducer // if we would have multiple state slices (bigger app), we would sert an obj with any keys of our choice and the values of those properties would then be different reducer functions.
-                                        //So we would create a map of reducers you could say, and this map is then set as a value for the main reducer and behind the scenes configureStore will emerge all those reducers into one big reducer. So it will merge them for us.
-}); 
 
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "decrement" });
+// // const reducer = (state = initialState, action) => {
+
+// //     if (action.type === "increment") {
+// //         return{
+// //         counter: state.counter +1,
+// //         showCounter: state.showCounter // have to set it, so we set to the value we currently having
+// //         };
+// //     }
+// //     if (action.type === "increase") {
+// //         return {
+// //             counter: state.counter + action.valueProp,
+// //             showCounter: state.showCounter
+// //     }
+// //     }
+
+// //     if (action.type === "decrement") {
+// //         return{
+// //         counter: state.counter -1,
+// //         showCounter: state.showCounter
+// //         };
+// //     }
+
+// //     if (action.type === "toggle") {
+// //         return {
+// //             showCounter: !state.showCounter, // we are setting it this way the opposite it was, so if it was true we are setting it false, and vica versa
+// //             counter:state.counter
+// //         };
+// //     }
+
+// //     return state;
+
+
+// // };
+
+// //const store = createStore(counterSlice.reducer); //This would be good this way for a smaller app.
+// //But if we have a bigger app with multiple stateslices, than we would have a problem as there can be only one reducer passed to create store
+// const store = configureStore({ //we pass a configuration obj, - which is expected. And then we set a reducer property => expected property by configureStore.
+//     reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }  //counterSlice.reducer // if we would have multiple state slices (bigger app), we would sert an obj with any keys of our choice and the values of those properties would then be different reducer functions.
+//                                         //So we would create a map of reducers you could say, and this map is then set as a value for the main reducer and behind the scenes configureStore will emerge all those reducers into one big reducer. So it will merge them for us.
+// }); 
+// /* store.dispatch({ type: "increment" });
+// store.dispatch({ type: "decrement" });
+//  */
+
+// export const counterActions = counterSlice.actions;
+// export const authActions = authSlice.actions;
+
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import counterReducer from './counter';
+import authReducer from './auth';
+
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
 export default store;
